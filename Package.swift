1e7e3473464567b0880a6c83d64b78e55612d945
Anka <runner@Mac-1734384139518.local>
2024-12-16 21:39:26 +0000

    KMP SPM package debug build for refs/heads/main

---
+++ Package.swift
@@ -0,0 +1,29 @@
+// swift-tools-version:5.8
+import PackageDescription
+
+// BEGIN KMMBRIDGE VARIABLES BLOCK (do not edit)
+let remoteKotlinUrl = "https://api.github.com/repos/touchlab/KMMBridgeAndroidDevFlowPublish/releases/assets/213778617.zip"
+let remoteKotlinChecksum = "bb3306c9f9999876a3772b8f59a8f8d6e87abd40081e716694c0bcf5df89ed36"
+let packageName = "kmplib"
+// END KMMBRIDGE BLOCK
+
+let package = Package(
+    name: packageName,
+    platforms: [
+        .iOS(.v14)
+    ],
+    products: [
+        .library(
+            name: packageName,
+            targets: [packageName]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: remoteKotlinUrl,
+            checksum: remoteKotlinChecksum
+        )
+        ,
+    ]
+)
---
+++ app/src/main/java/co/touchlab/kmp/devflow/MainActivity.kt
@@ -0,0 +1,48 @@
+package co.touchlab.kmp.devflow
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.touchlab.kmp.devflow.kmplib.whoAmIm
+import co.touchlab.kmp.devflow.ui.theme.DevFlowTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DevFlowTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = whoAmIm(),
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    DevFlowTheme {
+        Greeting("Android")
+    }
+}
